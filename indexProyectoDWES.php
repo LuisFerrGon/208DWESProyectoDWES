ee4493a5f5720a46249bd734bb998cc4d38b2a22
Luis Ferreras <luis.fergon.2@educa.jcyl.es>
2024-11-15 09:16:59 +0100

    15/11 primer commit

---
+++ indexProyectoDWES.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<!--Autor: Luis Ferreras González-->
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>Luis Ferreras</title>
+        <link type="text/css" rel="stylesheet" href="webroot/estilos.css">
+    </head>
+    <body>
+        <header>
+            <h1>Desarrollo Web en Entorno Servidor</h1>
+        </header>
+        <main>
+            <div>
+                <h2>T1. DESARROLLO WEB EN ENTORNO SERVIDOR</h2>
+                    <a href="doc/EstudioTema1.pdf" target="_blank">Estudio Teórico Tema 1</a>
+                    <a href="#">Javascript</a>
+                <h2>T2. CONFIGURACIÓN DEL ENTORNO DE DESARROLLO Y DEL ENTORNO DE EXPLOTACIÓN</h2>
+                    <a href="doc/EstudioTema2.pdf" target="_blank">Estudio Teórico Tema 2</a>
+                <h2>T3. CARACTERÍSTICAS DEL LENGUAJE PHP</h2>
+                    <a href="../208DWESProyectoTema3/indexProyectoTema3.php">Ejercicios Tema 3</a>
+                <h2>T4. TÉCNICAS DE ACCESO A DATOS EN PHP</h2>
+                    <a href="../208DWESProyectoTema4/indexProyectoTema4.php">Ejercicios Tema 4</a>
+                <h2>T5. DESARROLLO DE APLICACIONES WEB UTILIZANDO CÓDIGO EMBEBIDO</h2>
+                    <a href="#">Ejercicios Tema 5</a>
+                <h2>T6. PROGRAMACIÓN ORIENTADA A OBJETOS EN PHP</h2>
+                <h2>T7. PROGRAMACIÓN DE SERVICIOS WEB</h2>
+                <h2>T8. DESARROLLO DE APLICACIONES WEB HÍBRIDAS</h2>
+            </div>
+        </main>
+        <footer>
+            <a href="../index.html">Luis Ferreras</a>
+            <a href="https://github.com/LuisFerrGon/208DWESProyectoDWES">GitHub</a>
+        </footer>
+    </body>
+</html>
